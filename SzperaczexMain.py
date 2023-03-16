76ded15a035df5b86dda0e69684abe4a7e238ee5
Szymon <szymzwoo@gmail.com>
2023-03-16 21:47:32 +0100

    SZPER-3 Added Timer

---
+++ SzperaczexMain.py
@@ -0,0 +1,1 @@
+import SzperaczexTimer as timer
---
+++ global/SzperaczexTimer.py
@@ -0,0 +1,10 @@
+import time
+
+class SzperaczexTimer:
+    @staticmethod
+    def Tick():
+            time.sleep(500)
+            return True
+            
+            
+    
